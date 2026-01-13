06d98bab6cd650df9e13f1fc317738996d04fb6d
Julialutingye <venwa75@gmail.com>
2026-01-12 20:25:26 -0500

    Julia 01/12/2026

--- run_julia.py
+++ run_julia.py
@@ -1,34 +1,45 @@
 from pybricks.parameters import Color
-from pybricks.tools import run_task, wait
+from pybricks.tools import run_task, wait,multitask
 from pybricks.parameters import Axis, Direction, Port
-from robot_config import HUB, CENTER_ATTACHMENT
+from robot_config import HUB, CENTER_ATTACHMENT,BACK_ATTACHMENT, DRIVE_BASE
+from library import set_drivebase
 
 from pybricks.pupdevices import Motor
 from pybricks.parameters import Axis, Direction, Port
-# set up the attachments, if needed
 #CENTER_ATTACHMENT = Motor(Port.D, Direction.COUNTERCLOCKWISE, gears=[20,40])
 
 
-#this function tests control of center attachment.
 async def demo_center_attachment():
     await HUB.speaker.beep()
-    # for precision control, reset angle at the beginning.
-    # this function set the counter to be 0, 
-    # but not the absolute location (recall the markers on the motor and driving wheel)
+    await set_drivebase()
+    await wait(1000)
+    DRIVE_BASE.settings(straight_speed=400, straight_acceleration=1000, turn_rate=200, turn_acceleration=1000)
+    await DRIVE_BASE.straight(750)
+    await DRIVE_BASE.turn(180)
+    await DRIVE_BASE.straight(-167)
+    await DRIVE_BASE.turn(-102)
+    await wait(1000) 
     CENTER_ATTACHMENT.reset_angle(0)
-    await CENTER_ATTACHMENT.run_angle(90, -180) #speed, angle
-    await wait(500)
-    await CENTER_ATTACHMENT.run_angle(60, 180) #speed, angle
-    return 
-    await CENTER_ATTACHMENT.run_angle(180, -360) 
-
-    #repeat small movements
-    for i in range(5):
-        await wait(500)
-        await CENTER_ATTACHMENT.run_angle(180, 90) 
-        await wait(500)
-        await CENTER_ATTACHMENT.run_angle(180, -90)
- 
+    BACK_ATTACHMENT.reset_angle(0) 
+    DRIVE_BASE.settings(straight_speed=60, straight_acceleration=1000, turn_rate=200, turn_acceleration=1000)
+    await DRIVE_BASE.straight(142) 
+    await wait(1000) 
+    await CENTER_ATTACHMENT.run_angle(800, 400)
+    await BACK_ATTACHMENT.run_angle(800, 50)
+    await wait(1000)
+    await DRIVE_BASE.straight(-100)
+    DRIVE_BASE.settings(straight_speed=200, straight_acceleration=1000, turn_rate=200, turn_acceleration=1000)
+    await DRIVE_BASE.turn(54)
+    await CENTER_ATTACHMENT.run_angle(800, -350)
+    await DRIVE_BASE.straight(290)
+    await DRIVE_BASE.turn(-17)
+    await DRIVE_BASE.straight(70)
+    await multitask(CENTER_ATTACHMENT.run_angle(700, 500),DRIVE_BASE.turn(-5))
+    #await CENTER_ATTACHMENT.run_angle(800, 400)
+    await DRIVE_BASE.straight(-100)
+    await DRIVE_BASE.turn(35)
+    await DRIVE_BASE.straight(76)
+    await BACK_ATTACHMENT.run_angle(800, -100)
 
 if __name__ == "__main__":
     run_task(demo_center_attachment())
